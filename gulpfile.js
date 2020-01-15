0921b1ebd5f5a340d35237ce31afa1bc1ac5776b
--global <kocisek.martin@gmail.com>
2020-01-15 19:20:12 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+
+
+var paths = {
+    styles: {
+      src: 'src/sass/**/*.scss',
+      dest: 'dist/styles/'
+    },
+    scripts: {
+      src: 'src/js/**/*.js',
+      dest: 'dist/scripts/'
+    }
+  };
+
+  function clean() {
+    return del([ 'assets' ]);
+  }
+
+function styles() {
+    return gulp.src(paths.styles.src)
+     .pipe(sass().on('error', sass.logError))
+     .pipe(cleanCSS())
+     .pipe(rename({
+        basename: 'main',
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest(paths.styles.dest));
+  };
+
+function scripts() {
+    return gulp.src(paths.scripts.src, { sourcemaps: true })
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest(paths.scripts.dest));
+  };
+
+
+function watch() {
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.styles.src, styles);
+};
+
+var build = gulp.series(clean, gulp.parallel(styles, scripts));
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.build = build;
